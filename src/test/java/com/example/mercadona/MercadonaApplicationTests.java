01f3624d8d3b7a99060a92d05d47ebee19ccfc38
Hugo R <rhugo74@gmail.com>
2023-10-17 16:50:54 +0200

    Initial commit

---
+++ src/test/java/com/example/mercadona/MercadonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercadona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
